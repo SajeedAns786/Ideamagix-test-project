6a16eec2a14e566fbbb7d80cdb4d970b7ab9118e
Sajeed <saj.ans.rt19@rait.ac.in>
2023-01-20 01:00:12 +0530

    First Commit

---
+++ footer.php
@@ -0,0 +1,78 @@
+<div class="headings1">
+    <h3>| Why us?</h3>
+</div>
+<div class="headings2">
+    <h2>Leading EPC company working towards a sustainable tomorrow.</h2>
+
+</div>
+
+<div class="container" style="font-family: 'Gabriola',sans-serif; font-size: 30px;">
+    <ul>
+        <li>Experienced professional-60+</li>
+        <li>Accrediations
+        <li>ISO 9001:2015</li>
+        <li>ISO 14001:2015</li>
+        <li>OHSAS 45001:2018</li>
+        </li>
+        <li>Project delivered on time - 500MW+</li>
+        <li>Project under construction - 400MW+</li>
+        <li>Project under development.</li>
+    </ul>
+</div>
+
+
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+
+        <a class="navbar-brand" href=""><img src="images/Inergy-Logo.png" alt="" width="30%"></a>
+
+        <p class="footer-links">
+            <a href="index.php">Home</a>
+            ||
+            <a href="#">About Us</a>
+            ||
+            <a href="#">Servives</a>
+            ||
+            <a href="#">Careers</a>
+            ||
+            <a href="#">Contact Us</a>
+        </p>
+
+        <p class="footer-company-name">info@inergygroup.in</p>
+
+        <div>
+
+            <a href="#"><i class="fa-brands fa-square-facebook" style="padding-top: 15px;"></i></i></a>
+            <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="#"><i class="fa-brands fa-square-git"></i></a>
+
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+
+        <p>Contact Us</p>
+
+        <form action="#" method="post">
+
+            <input type="text" name="email" placeholder="Email">
+            <textarea name="message" placeholder="Message"></textarea>
+            <button>Send</button>
+
+        </form>
+
+    </div>
+
+</footer>
+<div class="container-fluid bg-dark text-light pl-5">
+    <div class="row pt-3">
+
+        <div class="col-md-12 text-center">
+
+            <p class="mt-5 mb-5">Made with &#x1F9E1; by Sajeed Ansari</p>
+        </div>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href=""><img src="images/Inergy-Logo.png" alt="" width="30%"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active txtcol" href="#"
+                style="color:blue;  font-family: 'Times New Roman', serif;">Home<span
+                    class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link " style="color:blue;  font-family: 'Times New Roman', serif;" href="#">About
+                Us</a>
+            <a class="nav-item nav-link txtcol" style="color:blue;  font-family: 'Times New Roman', serif;"
+                href="#">Services</a>
+            <a class="nav-item nav-link txtcol" style="color:blue;  font-family: 'Times New Roman', serif;"
+                href="#">Careers</a>
+            <a class="nav-item nav-link txtcol" href="#"
+                style="color:blue;  font-family: 'Times New Roman', serif;">Contact Us</a>
+
+        </div>
+    </div>
+</nav>
